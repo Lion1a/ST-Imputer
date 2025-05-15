956ac416d24911aa58092f90a771a079a8a0f257
Lion1a <95358590+Lion1a@users.noreply.github.com>
2025-05-15 16:13:42 +0800

    Add files via upload

---
+++ config/config.py
@@ -0,0 +1,11 @@
+class Config:
+    num_nodes = 108
+    feature_dim = 1
+    input_window = 108
+    output_window = 108
+    output_dim = 1
+    hidden_dim = 32
+    embed_dim = 10
+    num_layers = 2
+    cheb_order = 2
+    graph_layer = 1
